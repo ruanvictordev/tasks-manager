35aa9f05b02cd8c79ae070255aedda777aee6d4e
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-08-26 16:18:02 -0300

    :sparkles: [FEAT] Config Home Page Component.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>Home</div>
+      <section className='flex flex-col gap-y-20 justify-center items-center'>
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <h1 className='text-6xl text-center text-brand-tertiayColor font-bold'>Tasks Manager</h1>
+          <h2 className='text-slate-400 text-center text-lg'>Organize your tasks simply and online!</h2>
+          <a onClick={() => navigate("/register")} className='text-brand-tertiayColor underline cursor-pointer'>Register now!</a>
+        </div>
+
+        <div className='flex justify-center items-center'>
+          <img className='w-3/4 max-md:w-full' src="./home.svg" alt="" />
+        </div>
+      </section>
     </>
   )
 }
